0b7647680c7e664bb1675ae8748567dedd8cb036
chanhx <ichan.hocheung@gmail.com>
2023-08-27 19:09:19 +0800

    a0

--- exercises/cos_arceos/ulib/axstd/src/macros.rs
+++ exercises/cos_arceos/ulib/axstd/src/macros.rs
@@ -21,3 +21,11 @@ macro_rules! println {
         $crate::io::__print_impl(format_args!("{}\n", format_args!($($arg)*)));
     }
 }
+
+#[macro_export]
+macro_rules! println_prefix {
+    ($prefix:tt, $($arg:tt)*) => {
+        $crate::println!($prefix);
+        $crate::println!($($arg)*);
+    }
+}
